f09acbc640885bc61ab5f3ea8b2147e2154941fa
asmaahesham <aarfa@miu.edu>
2025-10-23 10:54:01 -0500

    remove cashe

--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecs_service_name" { value = aws_ecs_service.fastapi_service.name }
-output "db_endpoint" { value = aws_db_instance.postgres.address }
-output "ecr_url" { value = aws_ecr_repository.fastapi.repository_url }
--- terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-db_username = "postgres"
-db_password = "postgres"
-secret_key  = "trgE886yOieFXfAWEfKp8Az8wTLWFsw8jAf-LOGv81s="
-aws_region  = "us-east-1"
